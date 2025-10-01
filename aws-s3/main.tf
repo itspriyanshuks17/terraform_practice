be88136168a4920fda1d96c8d4caa74ef054edc2
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-10-01 12:58:10 +0530

    To make S3 name globally

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -6,42 +6,35 @@ terraform {
     }
 
     random = {
-        source  = "hashicorp/random"
-        version = "~> 3.0"
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-    region="ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "random_id" "random_id" {
-    byte_length = 8
+  byte_length = 8
 }
 
-#Creating S3 bucket
+# Creating S3 bucket with unique name
 resource "aws_s3_bucket" "demo-aws_s3_bucket" {
-    bucket = "demo-aws-s3-bucket-123fcd"
-    tags = {
-        Name = "demo-aws-s3-bucket"
-    }
+  bucket = "demo-aws-s3-bucket-${random_id.random_id.hex}"
+  tags = {
+    Name = "demo-aws-s3-bucket"
+  }
 }
 
-resource "aws_s3_bucket" "demo-aws_s3_bucket-1" {
-    bucket = "demo-aws-s3-bucket-${random_id.random_id.hex}"
-    tags = {
-        Name = "demo-aws-s3-bucket"
-    }
-}
-
-#Uploading objects on bucket
+# Uploading objects to the bucket
 resource "aws_s3_object" "bucket-data" {
-    bucket = aws_s3_bucket.demo-aws_s3_bucket.bucket
-    source = "./myfile.txt"
-    key = "mydata.txt"
+  bucket = aws_s3_bucket.demo-aws_s3_bucket.bucket
+  source = "./myfile.txt"
+  key    = "mydata.txt"
 }
 
 output "ame" {
-    value = random_id.random_id.hex
+  value = random_id.random_id.hex
 }
